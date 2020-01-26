276ac8464479da16d748d377b80f61aeb77bc626
JonasSiuksteris <33485554+JonasSiuksteris@users.noreply.github.com>
2020-01-26 15:27:53 +0200

    Added Unit Tests

---
+++ VideoPoker.UnitTests/HandEvaluationTests.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Video_Poker;
+
+namespace VideoPoker.UnitTests
+{
+    [TestClass]
+    public class HandEvaluationTests
+    {
+        [TestMethod]
+        public void Evaluate_RoyalFlush_Returns800()
+        {
+            var hand = new List<Card>
+            {
+                new Card(CardRank.King, CardSuit.Clubs),
+                new Card(CardRank.Ten, CardSuit.Clubs),
+                new Card(CardRank.Jack, CardSuit.Clubs),
+                new Card(CardRank.Queen, CardSuit.Clubs),
+                new Card(CardRank.Ace, CardSuit.Clubs)
+            };
+
+            var result = HandEvaluation.Evaluate(hand);
+
+            Assert.AreEqual(800, result);
+        }
+
+        [TestMethod]
+        public void Evaluate_StraightFlush_Returns50()
+        {
+            var hand = new List<Card>
+            {
+                new Card(CardRank.Six, CardSuit.Diamonds),
+                new Card(CardRank.Eight, CardSuit.Diamonds),
+                new Card(CardRank.Ten, CardSuit.Diamonds),
+                new Card(CardRank.Seven, CardSuit.Diamonds),
+                new Card(CardRank.Nine, CardSuit.Diamonds)
+            };
+
+            var result = HandEvaluation.Evaluate(hand);
+
+            Assert.AreEqual(50, result);
+        }
+
+        [TestMethod]
+        public void Evaluate_FourOfAKind_Returns25()
+        {
+            var hand = new List<Card>
+            {
+                new Card(CardRank.Jack, CardSuit.Diamonds),
+                new Card(CardRank.Jack, CardSuit.Spades),
+                new Card(CardRank.Jack, CardSuit.Hearts),
+                new Card(CardRank.Jack, CardSuit.Clubs),
+                new Card(CardRank.Nine, CardSuit.Diamonds)
+            };
+
+            var result = HandEvaluation.Evaluate(hand);
+
+            Assert.AreEqual(25, result);
+        }
+
+        [TestMethod]
+        public void Evaluate_FullHouse_Returns9()
+        {
+            var hand = new List<Card>
+            {
+                new Card(CardRank.Seven, CardSuit.Diamonds),
+                new Card(CardRank.Seven, CardSuit.Spades),
+                new Card(CardRank.Seven, CardSuit.Hearts),
+                new Card(CardRank.Ace, CardSuit.Clubs),
+                new Card(CardRank.Ace, CardSuit.Diamonds)
+            };
+
+            var result = HandEvaluation.Evaluate(hand);
+
+            Assert.AreEqual(9, result);
+        }
+
+        [TestMethod]
+        public void Evaluate_Flush_Returns6()
+        {
+            var hand = new List<Card>
+            {
+                new Card(CardRank.Two, CardSuit.Diamonds),
+                new Card(CardRank.Five, CardSuit.Diamonds),
+                new Card(CardRank.Ace, CardSuit.Diamonds),
+                new Card(CardRank.Jack, CardSuit.Diamonds),
+                new Card(CardRank.Nine, CardSuit.Diamonds)
+            };
+
+            var result = HandEvaluation.Evaluate(hand);
+
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void Evaluate_Straight_Returns4()
+        {
+            var hand = new List<Card>
+            {
+                new Card(CardRank.Five, CardSuit.Spades),
+                new Card(CardRank.Two, CardSuit.Clubs),
+                new Card(CardRank.Ace, CardSuit.Diamonds),
+                new Card(CardRank.Four, CardSuit.Diamonds),
+                new Card(CardRank.Three, CardSuit.Hearts)
+            };
+
+            var result = HandEvaluation.Evaluate(hand);
+
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void Evaluate_ThreeOfAKind_Returns3()
+        {
+            var hand = new List<Card>
+            {
+                new Card(CardRank.Seven, CardSuit.Spades),
+                new Card(CardRank.Seven, CardSuit.Clubs),
+                new Card(CardRank.Seven, CardSuit.Diamonds),
+                new Card(CardRank.Four, CardSuit.Diamonds),
+                new Card(CardRank.Three, CardSuit.Hearts)
+            };
+
+            var result = HandEvaluation.Evaluate(hand);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Evaluate_TwoPair_Returns2()
+        {
+            var hand = new List<Card>
+            {
+                new Card(CardRank.Seven, CardSuit.Spades),
+                new Card(CardRank.Seven, CardSuit.Clubs),
+                new Card(CardRank.King, CardSuit.Spades),
+                new Card(CardRank.King, CardSuit.Diamonds),
+                new Card(CardRank.Three, CardSuit.Hearts)
+            };
+
+            var result = HandEvaluation.Evaluate(hand);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Evaluate_JacksOrBetter_Returns1()
+        {
+            var hand = new List<Card>
+            {
+                new Card(CardRank.Jack, CardSuit.Spades),
+                new Card(CardRank.Seven, CardSuit.Clubs),
+                new Card(CardRank.Two, CardSuit.Spades),
+                new Card(CardRank.Five, CardSuit.Diamonds),
+                new Card(CardRank.Ten, CardSuit.Hearts)
+            };
+
+            var result = HandEvaluation.Evaluate(hand);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Evaluate_Nothing_Returns0()
+        {
+            var hand = new List<Card>
+            {
+                new Card(CardRank.Three, CardSuit.Spades),
+                new Card(CardRank.Seven, CardSuit.Clubs),
+                new Card(CardRank.Two, CardSuit.Spades),
+                new Card(CardRank.Five, CardSuit.Diamonds),
+                new Card(CardRank.Ten, CardSuit.Hearts)
+            };
+
+            var result = HandEvaluation.Evaluate(hand);
+
+            Assert.AreEqual(0, result);
+        }
+    }
+}
---
+++ VideoPoker.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VideoPoker.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VideoPoker.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("272f081b-eb8c-48c2-aa52-fe4751ca7733")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
